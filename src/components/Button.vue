52823d40ae1c010845408db69c68a858a4e20c63
p.borowiec <p.borowiec@praktyki.ideo.pl>
2022-11-28 14:16:02 +0100

    add navigation bar

---
+++ src/components/Button.vue
@@ -0,0 +1,26 @@
+<template>
+    <button>{{ props.name }}</button>
+</template>
+<script setup lang="ts">
+const props = defineProps<{
+    name?: string
+    disabled?: boolean
+}>()
+</script>
+<style scoped lang="scss">
+button {
+    padding: 12px 24px;
+    border: 2px solid black;
+    border-radius: 8px;
+    font-weight: bold;
+    font-size: 14px;
+    cursor: pointer;
+}
+.light {
+    background: transparent;
+}
+.dark {
+    background: black;
+    color: white;
+}
+</style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,37 +1,96 @@
 <template>
     <nav>
         <div class="wrapper">
-            <img src="../assets/logo.svg" alt="logo" />
-            <p @click="state.menuVisible = !state.menuVisible">menu</p>
+            <div class="inner">
+                <img class="logo" src="../assets/logo.svg" alt="logo" />
+                <ul v-if="state.wideDevice">
+                    <li>Episodes</li>
+                    <li>About</li>
+                    <li>More</li>
+                </ul>
+            </div>
+
+            <div class="button-wrapper" v-if="state.wideDevice">
+                <Button class="light" name="RECENT EPISODES" />
+                <Button class="dark" name="SUBSCRIBE" />
+            </div>
+            <img
+                src="../assets/icons/menu-line.svg"
+                alt="Menu"
+                class="menu-icon"
+                v-if="!state.wideDevice"
+                @click="state.menu = !state.menu"
+            />
         </div>
-        <menu v-if="state.menuVisible">
+        <menu v-if="state.menu && !state.wideDevice">
             <ul>
                 <li>Episodes</li>
                 <li>About</li>
                 <li>More</li>
+                <Button class="light" name="RECENT EPISODES" />
+                <Button class="dark" name="SUBSCRIBE" />
             </ul>
         </menu>
     </nav>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue'
+import { reactive } from 'vue'
+import Button from './Button.vue'
 
-const state = ref({
-    menuVisible: false,
+const state = reactive({
+    menu: false,
+    wideDevice: false,
 })
+
+window.addEventListener('resize', handleResize)
+
+function handleResize() {
+    if (window.innerWidth >= 1150) {
+        state.wideDevice = true
+    } else {
+        state.wideDevice = false
+    }
+    console.log(window.innerWidth)
+}
+
+handleResize()
 </script>
 
 <style scoped lang="scss">
 nav {
     background: var(--skinny);
-    padding: 24px;
     display: flex;
     flex-direction: column;
     justify-content: space-between;
+    font-weight: bold;
+    font-size: 16px;
+    padding: 24px 10vw;
 
-    img {
+    @media (min-width: 1150px) {
+        padding: 24px 140px;
+    }
+
+    .logo {
         width: 64px;
+        cursor: pointer;
+    }
+    .menu-icon {
+        width: 35px;
+        cursor: pointer;
+    }
+
+    ul {
+        justify-content: center;
+        align-items: center;
+        list-style-type: none;
+        flex-direction: row;
+        display: flex;
+        gap: 60px;
+
+        li {
+            cursor: pointer;
+        }
     }
     .wrapper {
         display: flex;
@@ -39,15 +98,27 @@ nav {
         justify-content: space-between;
         align-items: center;
         width: 100%;
+
+        ul {
+            flex-direction: row;
+        }
+
+        .inner {
+            display: flex;
+            gap: 124px;
+        }
+
+        .button-wrapper {
+            display: flex;
+            gap: 20px;
+        }
     }
 
     menu {
         margin-top: 20px;
 
         ul {
-            list-style-type: none;
             flex-direction: column;
-            display: flex;
             gap: 20px;
         }
     }
